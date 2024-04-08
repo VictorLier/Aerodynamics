d7b9bf00f0fd4899c874855bdc5c023402c34fcd
VictorLier <victor@lier.dk>
2024-04-08 22:41:50 +0200

    Tæt på done

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -111,19 +111,20 @@ class EllipticAirfoil():
         # Get xfoil data - list of angles [radians], zero lift angle of attack [radians], slope of lift curve, friction drag coefficient
         self.AOA, self.alpha0, self.m0, self.cd_friction = get_xfoil_data(self.airfoil_name, self.AOA_start, self.AOA_end, self.AOA_step, self.Re)
 
+
         # List of theta values
         dpi = np.pi/self.nA
         self.theta = np.linspace(dpi, np.pi-dpi, self.nA)
 
         # Cord function - root cord c0 = 1
-        c0 = 1
-        self.c = np.sin(self.theta) * c0
+        self.c0 = 1
+        self.c = np.sin(self.theta) * self.c0
 
         # Wing span
-        self.b = self.AR * np.pi * c0 / 4
+        self.b = self.AR * np.pi * self.c0 / 4
 
         # Wing area
-        self.S = np.pi * c0/2 * self.b/2
+        self.S = np.pi * self.c0/2 * self.b/2
         
         # Calculate Fourier coefficients
         # initialize A for each AOA
@@ -132,6 +133,14 @@ class EllipticAirfoil():
         # Calculate A vector for each AOA and store in A
         for i, AOA in enumerate(self.AOA):
             self.A[:,i] = solve_fourier_coefs(nA=self.nA, m0=self.m0, b=self.b, c=self.c, alpha_L0=self.alpha0, alpha=AOA, theta_array=self.theta)
+
+    def friction_drag(self) -> np.ndarray:
+        '''
+        Calculate the friction drag
+        return: Friction drag, Angle of attacks in degrees
+        '''
+        cd_fric = self.cd_friction * self.S / self.b
+        return cd_fric, self.AOA*180/np.pi
 
     def analytical_cl(self) -> np.ndarray:
         '''
@@ -191,7 +200,7 @@ class EllipticAirfoil():
         return ai*180/np.pi, self.AOA*180/np.pi
 
 class ConstantAirfoil():
-    def __init__(self, AR, airfoil_name = "NACA4415", Re = 6e6, nA = 11, AOA_start = 0, AOA_end = 10, AOA_step = 5):
+    def __init__(self, AR, airfoil_name = "NACA4415", Re = 6e6, nA = 11, AOA_start = 0, AOA_end = 10, AOA_step = 5, twist = False):
         '''
         airfoil_name: Airfoil section name eg. NACA4415
         Re: Reynolds number
@@ -225,6 +234,7 @@ class ConstantAirfoil():
 
         # Wing area
         self.S = self.c * self.b
+
         
         # Calculate Fourier coefficients
         # initialize A for each AOA
@@ -233,6 +243,8 @@ class ConstantAirfoil():
         # Calculate A vector for each AOA and store in A
         for i, AOA in enumerate(self.AOA):
             self.A[:,i] = solve_fourier_coefs(nA=self.nA, m0=self.m0, b=self.b, c=self.c, alpha_L0=self.alpha0, alpha=AOA, theta_array=self.theta)
+
+
 
     def ai(self) -> np.ndarray:
         '''
@@ -272,10 +284,91 @@ class ConstantAirfoil():
         cDi = np.pi * self.AR * cDi_sum
         return cDi, self.AOA*180/np.pi
 
+class TwistAirfoil():
+    def __init__(self, AR=6, airfoil_name = "NACA4415", Re = 6e6, alpha_c = 4, alpha_tip = 0, nA = 50):
+        self.AR = AR
+        self.airfoil_name = airfoil_name
+        self.nA = nA
+        self.Re = Re
+        self.alpha_c = alpha_c * np.pi / 180
+        self.alpha_tip = alpha_tip * np.pi / 180
+        self.b = self.AR
+
+        dpi = np.pi/self.nA
+        self.theta = np.linspace(dpi, np.pi-dpi, self.nA)
+
+        # Get xfoil data - list of angles [radians], zero lift angle of attack [radians], slope of lift curve, friction drag coefficient
+        self.AOA, self.alpha0, self.m0, self.cd_friction = get_xfoil_data(airfoil_name=self.airfoil_name, AOA_start=0, AOA_end=8, AOA_step=2, Re=self.Re)
+
+
+
+        self.alpha = self.alpha_c + (self.alpha_tip - self.alpha_c) * abs(np.cos(self.theta))
+    
+        # solve for fourier coefficients
+
+        # Initialize ordinate vector - o
+        o = - self.alpha + self.alpha0
+
+        # Initialize coefficient matrix - B
+        B = np.zeros((nA, nA))
+
+        # Fill in B matrix -  Silde 26 week 8
+        for j, theta in enumerate(self.theta):
+            for i in range(nA):
+                B[j, i] = (-4*self.AR*np.sin((1+i)*theta) / (self.m0) - (1+i)*(np.sin((1+i)*theta)/np.sin(theta))) # Silde 26 week 8
+
+        # Solve for A
+        self.A = np.linalg.solve(B, o)
+        self.A[abs(self.A) < 1e-15] = 0
+
+
+    def local_ai(self):
+        ai = np.zeros(len(self.theta))
+        for i, theta in enumerate(self.theta):
+            ai_sum = 0
+            for n, A in enumerate(self.A):
+                sum = (n+1)*A * np.sin((n+1)*theta) / np.sin(theta)
+                ai_sum = ai_sum + sum
+            ai[i] = ai_sum
+
+        return self.theta, ai
+
+    def local_cl(self):
+        cl = np.zeros(len(self.theta))
+        for i, theta in enumerate(self.theta):
+            cl_sum = 0
+            for n, A in enumerate(self.A):
+                sum = A * np.sin((n+1)*theta)
+                cl_sum = cl_sum + sum
+            cl[i] = 4 * self.AR * cl_sum
+
+        return self.theta, cl
+    
+
+    def local_Cdi(self):
+        cl = self.local_cl()[1]
+        ai = self.local_ai()[1]
+        CDi = cl * ai
+
+        return self.theta, CDi
+
+    def circulation(self):
+        circulation = np.zeros(len(self.theta))
+        
+        for i, theta in enumerate(self.theta):
+            circulation_sum = 0
+            for n, A in enumerate(self.A):
+                sum = A * np.sin((n+1)*theta)
+                circulation_sum = circulation_sum + sum
+            circulation[i] = 2 * circulation_sum
+        
+        return self.theta, circulation
+
+
 
 if __name__ == "__main__":
 
-    if False: # Task 1
+    if True: # Task 1
         AR = [4, 6, 8, 10, 10000]
         # AR = [10]
         Foils = []
@@ -289,7 +382,7 @@ if __name__ == "__main__":
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Lift coefficient")
-        plt.title("Lift coefficient vs. Angle of attack for different aspect ratios")
+        plt.title("Lift coefficient vs. Angle of attack")
         plt.grid(True)
 
         # # Analytical Cl
@@ -299,7 +392,7 @@ if __name__ == "__main__":
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Lift coefficient")
-        plt.title("Analytical lift coefficient vs. Angle of attack for different aspect ratios")
+        plt.title("Analytical lift coefficient vs. Angle of attack")
         plt.grid(True)
 
         # General Cdi
@@ -309,7 +402,7 @@ if __name__ == "__main__":
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Induced drag coefficient")
-        plt.title("Induced drag coefficient vs. Angle of attack for different aspect ratios")
+        plt.title("Induced drag coefficient vs. Angle of attack")
         plt.grid(True)
 
         # Analytical Cdi
@@ -319,7 +412,7 @@ if __name__ == "__main__":
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Induced drag coefficient")
-        plt.title("Analytical induced drag coefficient vs. Angle of attack for different aspect ratios")
+        plt.title("Analytical induced drag coefficient vs. Angle of attack")
         plt.grid(True)
 
 
@@ -330,25 +423,25 @@ if __name__ == "__main__":
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Induced angle of attack")
-        plt.title("Induced angle of attack vs. Angle of attack for different aspect ratios")
+        plt.title("Induced angle of attack vs. Angle of attack")
         plt.grid(True)
 
-        # # AOA
+        # Friction drag
         plt.figure()
         for foil in Foils:
-            plt.plot(foil.AOA*180/np.pi, foil.cd_friction)
+            plt.plot(foil.friction_drag()[1], foil.friction_drag()[0])
         plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
         plt.xlabel("Angle of attack (degrees)")
         plt.ylabel("Friction drag coefficient")
-        plt.title("Friction drag coefficient vs. Angle of attack for different aspect ratios")
+        plt.title("Friction drag coefficient vs. Angle of attack")
         plt.grid(True)
 
 
 
         plt.show()
 
-    if True: # Task 2
-        if True: # Part A
+    if False: # Task 2
+        if False: # Part A
             AR = [4, 6, 8, 10, 10000]
             # AR = [10]
             Foils = []
@@ -362,7 +455,7 @@ if __name__ == "__main__":
             plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
             plt.xlabel("Theta")
             plt.ylabel("Induced angle of attack")
-            plt.title("Induced angle of attack vs. Theta for different aspect ratios and angle of attack = 0 degrees")
+            plt.title("Induced angle of attack vs. Theta angle of attack = 0 degrees")
             plt.grid(True)
 
             # AOA 5
@@ -372,7 +465,7 @@ if __name__ == "__main__":
             plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
             plt.xlabel("Theta")
             plt.ylabel("Induced angle of attack")
-            plt.title("Induced angle of attack vs. Theta for different aspect ratios and angle of attack = 5 degrees")
+            plt.title("Induced angle of attack vs. Theta angle of attack = 5 degrees")
             plt.grid(True)
 
             # AOA 10
@@ -382,7 +475,7 @@ if __name__ == "__main__":
             plt.legend(["AR=4", "AR=6", "AR=8", "AR=10", "AR=inf"])
             plt.xlabel("Theta")
             plt.ylabel("Induced angle of attack")
-            plt.title("Induced angle of attack vs. Theta for different aspect ratios and angle of attack = 10 degrees")
+            plt.title("Induced angle of attack vs. Theta angle of attack = 10 degrees")
             plt.grid(True)
             
             plt.show()
@@ -417,9 +510,57 @@ if __name__ == "__main__":
 
             plt.show()
     
+    if False: # Task 4
+        angles = [0, 2, 4, 6, 8]
+        Foils = []
+        for angle in angles:
+            Foils.append(TwistAirfoil(alpha_tip=angle))
+        
+        # ai
+        plt.figure()
+        for foil in Foils:
+            plt.plot(foil.local_ai()[0], foil.local_ai()[1]*180/np.pi)
+        plt.legend(["0", "2", "4", "6", "8"])
+        plt.xlabel("Theta")
+        plt.ylabel("Induced angle of attack")
+        plt.title("Induced angle of attack vs. Theta for different tip angles")
+        plt.grid(True)
+
+        # cl
+        plt.figure()
+        for foil in Foils:
+            plt.plot(foil.local_cl()[0], foil.local_cl()[1])
+        plt.legend(["0", "2", "4", "6", "8"])
+        plt.xlabel("Theta")
+        plt.ylabel("Lift coefficient")
+        plt.title("Lift coefficient vs. Theta for different tip angles")
+        plt.grid(True)
+
+        # Cdi
+        plt.figure()
+        for foil in Foils:
+            plt.plot(foil.local_Cdi()[0], foil.local_Cdi()[1])
+        plt.legend(["0", "2", "4", "6", "8"])
+        plt.xlabel("Theta")
+        plt.ylabel("Induced drag coefficient")
+        plt.title("Induced drag coefficient vs. Theta for different tip angles")
+        plt.grid(True)
+
+        # circulation
+        plt.figure()
+        for foil in Foils:
+            plt.plot(foil.circulation()[0], foil.circulation()[1])
+        plt.legend(["0", "2", "4", "6", "8"])
+        plt.xlabel("Theta")
+        plt.ylabel("Circulation")
+        plt.title("Circulation vs. Theta for different tip angles")
+        plt.grid(True)
+
+        plt.show()
+
 
     if False: # test
-        alpha, alpha0, m0, cd_friction = get_xfoil_data("NACA4415", -6, 10, 0.5, 6e6)
+        Test = TwistAirfoil()
         
 
 
